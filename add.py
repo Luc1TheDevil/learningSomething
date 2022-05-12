3d04c43b438b0acd10318f080afb81643e9302a1
Luc1TheDevil <momprakash2001@gmail.com>
2022-05-12 23:23:33 +0530

    Grade calc using oops

--- add.py
+++ add.py
@@ -1,5 +1,30 @@
-a=10
-b=20
-d=50
-c=a+b
-print(c)
+class students:
+
+    def __init__(self,roll,name,marks_list):
+        self.roll=roll
+        self.name=name
+        self.marks_list=marks_list
+    def calculate_percentage(self):
+        percentage=sum(self.marks_list)//len(self.marks_list)
+        return percentage
+    def grade(self):
+        percentage=self.calculate_percentage()
+        if percentage>=90:
+            return 'A'
+        elif percentage>=60:
+            return 'B'
+        elif percentage>=50:
+            return  'C'
+        else:
+            return 'F'
+
+roll=int(input("ENTER THE ROLL NUM:"))
+name=input("ENTER THE ROLL NAME:")
+count=int(input("no of sub:"))
+
+marks=[]
+for i in range(count):
+    marks.append(int(input()))
+s=students(roll,name,marks)
+print(s.calculate_percentage())
+print(s.grade())
